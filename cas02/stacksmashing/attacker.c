7e931638473cd7e0a56df43cde6f9e1a22a4271d
b-luka <lukaorca@gmail.com>
2026-04-02 13:09:48 +0200

    2. termin 1. cas

---
+++ cas02/stacksmashing/attacker.c
@@ -0,0 +1,65 @@
+/*
+#include <stdlib.h>
+#include <string.h>
+extern void service(char* param, long param_length);
+extern void worm_foo();
+
+int main(int argc, char** argv) {
+    long param_length = 16;
+    char *param = (char*)calloc(param_length, sizeof(char));
+
+    long worm_foo_address = (long)&worm_foo;
+
+    for (int i = 0; i < 6; i++) {
+        //memcpy(param + i * 8, "\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA", 8);
+        memcpy(param + i * 8, &worm_foo_address, 8);
+    }
+
+    service(param, param_length);
+}
+*/
+
+#include <string.h>
+#include <stdlib.h>
+
+extern void service_foo(char *param, long param_length);
+extern void worm_foo();
+
+char *value_to_array(long value);
+
+enum Constants
+{
+  QUAD_WORDS_NUM = 6,
+  QUAD_WORD_SIZE = 8 // 8 bytes
+};
+
+int main()
+{
+  long arg_length = QUAD_WORDS_NUM * QUAD_WORD_SIZE;
+  char *arg = (char *)calloc(arg_length, sizeof(char));
+
+  char *worm_foo_address_array = value_to_array((long)&worm_foo);
+  for (int i = 0; i < QUAD_WORDS_NUM; i++)
+  {
+    memcpy(arg + i * QUAD_WORD_SIZE, worm_foo_address_array, QUAD_WORD_SIZE);
+  }
+  free(worm_foo_address_array);
+
+  service_foo(arg, arg_length);
+
+  return 0;
+}
+
+// create little endian array representation of given 8 byte value
+// 0x7766554433221100 ---> [0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77]
+char *value_to_array(long value)
+{
+  char *array = (char *)calloc(QUAD_WORD_SIZE, sizeof(char));
+  for (int i = 0; i < QUAD_WORD_SIZE; i++)
+  {
+    char temp = value & 0xFF;
+    memcpy(array + i, &temp, 1);
+    value >>= 8; // shift 8 bits (single byte)
+  }
+  return array;
+}
---
+++ cas02/stacksmashing/victim.c
@@ -0,0 +1,87 @@
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void service(char* param, long param_length) {
+    long stack_var_1 = 0x123;
+    char buffer[16];
+    long stack_var_2 = 0x456;
+
+    printf("stack var 1: %lx\n", stack_var_1);
+    printf("stack var 2: %lx\n", stack_var_2);
+
+    memcpy(buffer, param, param_length);
+
+    printf("stack var 1: %lx\n", stack_var_1);
+    printf("stack var 2: %lx\n", stack_var_2);
+}
+
+void worm_foo() {
+    printf("---------------worm--------------");
+    exit(0);
+}
+*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void worm_foo();
+
+/*
+ *
+ *             HIGH ADDRESSES
+ * 
+ * 
+ *          | ???????????????? |   0x7fffffffde18: [rbp + 0x18] (previous frame)
+ *          | ???????????????? |   0x7fffffffde10: [rbp + 0x10] (previous frame)
+ * ------------------------------------------------------------------------------------
+ *          | 00005555555551fc |   0x7fffffffde08: [rbp + 0x08] (return address)
+ *  RBP --> | 00007fffffffde30 |   0x7fffffffde00: [rbp + 0x00] (old rbp value)
+ *          | 0000000000000111 |   0x7fffffffddf8: [rbp - 0x08] (stack_variable_1)
+ *          | 0000000000000222 |   0x7fffffffddf0: [rbp - 0x10] (stack_variable_2)
+ *          | aaaaaaaaaaaaaaaa |   0x7fffffffdde8: [rbp - 0x18] (stack_buffer[15-08])
+ *          | aaaaaaaaaaaaaaaa |   0x7fffffffdde0: [rbp - 0x20] (stack_buffer[07-00])
+ *          | 00005555555592a0 |   0x7fffffffddd8: [rbp - 0x28] (param)
+ *  RSP --> | 0000000000000010 |   0x7fffffffddd0: [rbp - 0x30] (param_length)
+ *
+ * 
+ *              LOW ADDRESSES
+ */
+
+void service_foo(char *param, long param_length)
+{
+  long stack_variable_1 = 0x111;
+  char stack_buffer[16];
+  long stack_variable_2 = 0x222;
+
+  // ASLR (Address Space Layout Randomization)
+  printf("&stack_buffer = %p\n", &stack_buffer);
+  printf("&worm_foo     = %p\n", &worm_foo);
+
+  printf(
+    "\nbefore memcpy() :"
+    "\nstack_variable_1 = %#lx"
+    "\nstack_variable_2 = %#lx\n",
+    stack_variable_1,
+    stack_variable_2
+  );
+
+  // some mocked processing: begin
+  memcpy(stack_buffer, param, param_length);
+  // some mocked processing: end
+
+  printf(
+    "\nafter memcpy() :"
+    "\nstack_variable_1 = %#lx"
+    "\nstack_variable_2 = %#lx\n",
+    stack_variable_1,
+    stack_variable_2
+  );
+}
+
+void worm_foo()
+{
+  printf("\n***** worm_foo() *****\n");
+  exit(0);
+}
